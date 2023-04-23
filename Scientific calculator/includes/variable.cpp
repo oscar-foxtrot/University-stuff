f36851a19286e55057ec09e382aa92dea1862b1c
oscar-foxtrot <93640558+oscar-foxtrot@users.noreply.github.com>
2023-04-23 18:17:30 +0300

    Create variable.cpp

---
+++ Scientific calculator/includes/variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "variable.hpp"
+
+Variable::Variable() {}
+
+Variable::Variable(std::string val): value(val) {}
+
+std::ostream &operator<<(std::ostream &os, const Variable &var) 
+{
+    return std::cout << var.value;
+}
+
+std::string Variable::get_value() const 
+{
+    return value;
+}    
